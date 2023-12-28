d602359f2a97ec3893b3a8c92aba947ab9fb7fc5
ZEQUANR <675897584@qq.com>
2023-12-28 14:22:31 +0800

    Fix: add navigation bar search function

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress"
 import { getDirname, path } from "@vuepress/utils";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js"
 
 const __dirname = getDirname(import.meta.url);
@@ -26,6 +27,24 @@ export default defineUserConfig({
       })();
         `
     ]
+  ],
+
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
   ],
 
   theme,
